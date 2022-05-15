a0cbb1f64a8cc98c4ea4acc757570b4a06deb413
Sasha Pavlenko <380971520240@gmail.com>
2022-05-15 17:03:48 +0300

    sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+const headers = list.querySelectorAll('li');
+
+for (const item of headers) {
+  const span = document.createElement('span');
+
+  span.append(item.firstChild);
+  item.prepend(span);
+}
+
+list.addEventListener('click', e => {
+  const item = e.target;
+  const nestedList = item.parentElement.querySelector('ul');
+
+  if (!nestedList || item.tagName !== 'SPAN') {
+    return;
+  };
+
+  if (nestedList.hidden) {
+    nestedList.removeAttribute('hidden');
+  } else {
+    nestedList.hidden = `true`;
+  }
+});
